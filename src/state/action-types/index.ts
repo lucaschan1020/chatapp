fe26320adf39d377c25fe1f070f3c67e89b75fd7
Lucas <lucaschan1020@hotmail.com>
2022-02-24 02:39:56 +0800

    delete unused redux action creator and types

--- src/state/action-types/index.ts
+++
@@ -1,7 +0,0 @@
-export enum ActionType {
-  AddPrivateChannels = 'ADD_PRIVATE_CHANNELS',
-  ChangeCurrentChat = 'CHANGE_CURRENT_CHAT',
-  AddChatMessage = 'ADD_CHAT_MESSAGE',
-  SendChatMessage = 'SEND_CHAT_MESSAGE',
-  ReceiveChatMessage = 'RECEIVE_CHAT_MESSAGE',
-}
